98902cdeae9191a5b48a2a2c2e5fb66fb6befd4d
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-02-22 16:35:51 -0800

    working on logic to show times per date and remove empty rows

--- client/app/event/event.show.controller.js
+++ client/app/event/event.show.controller.js
@@ -1,19 +1,28 @@
 angular.module('gobeApp')
   .controller('EventShowCtrl', function ($scope, $stateParams, Event) {
-    var timeArray = ["8am-12pm", "12pm-5pm", "5pm-8pm"];
+    var timeArray = ["8am - 12pm", "12pm - 5pm", "5pm - 8pm"];
 
     $scope.checkTime = function(time, index) {
-      if(time){
-        if(index === 0){
+
+        if(time && (index === 0)){
           return timeArray[0];
         }
-         if(index === 1){
+        else {
+          return null
+        }
+        if(time && (index === 1)){
           return timeArray[1];
         }
-         if(index === 2){
+        else {
+          return null
+        }
+        if(time && (index === 2)){
           return timeArray[2];
         }
-      }
+        else {
+          return null
+        }
+
     };
     $scope.event = Event.get({id: $stateParams.id});
   });
