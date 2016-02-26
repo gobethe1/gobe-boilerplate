3cc76dd6441ace809db73bb208dc5024de4940f3
Greg Rock <grock006@gmail.com>
2016-02-25 16:47:20 -0800

    nodemailer working with loop

--- server/api/event/event.zipcodeMatch.emailer.js
+++ server/api/event/event.zipcodeMatch.emailer.js
@@ -6,75 +6,58 @@ var _     = require('lodash');
 var Event = require('./event.model');
 var Group = require('../group/group.model');
 var nodemailer = require('nodemailer');
+var GodaddyPassword = process.env.GODADDY_PASSWORD;
+var GodaddySMTP = process.env.GODADDY_SMTP;
 
 function matchZipCode(event){
-	var groupEmailArr = [];
-
-	// console.log("match zip code fires")
-	// console.log(event.zipCode);
 
 	async.waterfall([
 	  function(done) {
-	   	// console.log("inside waterfall")
 	    Group.find({ zipCode: event.zipCode }, function(err, group) {
 	      if (!group) {
 	      	console.log(err)
 	        return res.status(404).send('There are no zipcode matches.');
 	      }
-	      else{
-	      	console.log("matching groups")
-	      	// console.log(group)
-	      	group.map(function(value){
-		      	console.log(value.email)
-		      	groupEmailArr.push(value.email)
-		      	console.log("inside groupEmailArr")
-		      	console.log(groupEmailArr)
-	     	})
-	      }
-
-	     
-
-	      // user.save(function(err) {
-	        done(err, group);
-	      // });
-	    
+	       done(err, group);
 	    });
 	  },
 	  function(group, done) {
 
-	  	console.log("outside groupEmailArr inside email yo")
-	  	console.log(groupEmailArr)
+	    group.map(function(value){
 
-	  	// "Godaddy": {
-	  	//         "host": "smtpout.secureserver.net",
-	  	//         "port": 25
-	  	//     },
-	    
-	    var transporter = nodemailer.createTransport({
-	      host: "localhost",
-	      port: 25,
-	      domain: "localhost",
-	      auth: {
-	        user: 'hello@gobethe1.com',
-	        pass: 'getshitdone123!'
-	      }
-	    });
-	    var mailOptions = {
-	      to: 'cassie.purtlebaugh@gmail.com',
-	      from: 'hello@gobethe1.com',
-	      subject: 'Local Move In Opportunity',
-	      text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
-	        'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
-	        // 'http://' + req.headers.host + '/reset/' + token + '\n\n' +
-	        'If you did not request this, please ignore this email and your password will remain unchanged.\n'
-	    };
-	    transporter.sendMail(mailOptions, function(err) {
-	      // return res.status(200).send('An e-mail has been sent to ' + user.email + ' with further instructions.');
-	      done(err, 'done');
-	    });
+		    var transporter = nodemailer.createTransport({
+		      host: GodaddySMTP,
+		      port: 25,
+		      auth: {
+		        user: 'hello@gobethe1.com',
+		        pass: GodaddyPassword
+		      }
+		    });
+		    
+		    var mailOptions = {
+		      to: value.email,  //'cassie.purtlebaugh@gmail.com',
+		      from: 'hello@gobethe1.com',
+		      subject: 'Are You Available?',
+		      text: 'Hello ' + value.firstName + ', we matched you with a move-in party of a homeless vet in your neighborhood!\n\n' +
+		        'Check out the dates and times and let us know if you are available:\n\n' +
+		        'http://localhost:9000/event/' + event._id + '/confirmation/' + value._id
+		    };
+		
+		    transporter.sendMail(mailOptions, function(err) {
+		    	console.log("inside sendMail err")
+		    	console.log(err)
+		      // return res.status(200).send('An e-mail has been sent to ' + user.email + ' with further instructions.');
+		    });
+		
+		});
+
+	    done('done');
+
+	  
 	  }
 	], function(err) {
 	  // if (err) return next(err);
+	  console.log("last error")
 	  console.log(err)
 	  // res.redirect('/forgot');
 	});
