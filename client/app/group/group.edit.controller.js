91f1a85961fa28e084a28e60571ba99ed51a5282
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-03-02 14:14:02 -0800

    edit-group update sending to backend

--- client/app/group/group.edit.controller.js
+++ client/app/group/group.edit.controller.js
@@ -2,6 +2,23 @@
 
 angular.module('gobeApp')
   .controller('GroupEditCtrl', function ($scope, $state, $stateParams, Group, groupEdit) {
-    $scope.group = groupEdit;
-    console.log($scope.group);
+    $scope.newGroup = groupEdit;
+    console.log($scope.newGroup);
+
+    $scope.updateGroup = function addGroup(form) {
+      var data = $scope.newGroup;
+      $scope.submitted = true;
+         if(form.$valid){
+             Group.update({id: $stateParams.id }, data,
+               function(data){
+                  $state.go('group.list')
+                 }),
+                 function(err){
+                  $scope.addGroupError = "Looks like something went wrong! Please try again"
+                 }
+               }
+         else{
+             document.body.scrollTop = document.documentElement.scrollTop = 0;
+         }
+    };
   });
