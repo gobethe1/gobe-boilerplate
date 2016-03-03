13711581f0f8d60d82b71c1658cec39b6ae2d3c6
Ed Sahakian <ed.sahakian@gmail.com>
2016-03-03 09:58:06 -0800

    set port back to 9000 for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function (grunt) {
     },
     express: {
       options: {
-        port: process.env.PORT || 8080
+        port: process.env.PORT || 9000
       },
       dev: {
         options: {
@@ -208,7 +208,7 @@ module.exports = function (grunt) {
         options: {
           nodeArgs: ['--debug-brk'],
           env: {
-            PORT: process.env.PORT || 8080
+            PORT: process.env.PORT || 9000
           },
           callback: function (nodemon) {
             nodemon.on('log', function (event) {
