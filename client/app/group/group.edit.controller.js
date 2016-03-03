1ef93c8a82cbefd3596f02e2dfdb7bcf086539f9
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-03-03 10:50:41 -0800

    add hover to emailist field in group-edit.html

--- client/app/group/group.edit.controller.js
+++ client/app/group/group.edit.controller.js
@@ -4,12 +4,11 @@ angular.module('gobeApp')
   .controller('GroupEditCtrl', function ($scope, $state, $stateParams, Group, groupEdit) {
     $scope.newGroup = groupEdit;
     $scope.emailList = $scope.newGroup.emailList;
-    // console.log($scope.newGroup);
+    $scope.hover = true;
+
 
     $scope.updateEmail = function updateEmail(){
       $scope.emailList.push($scope.email);
-      // console.log('emailList');
-      // console.log($scope.emailList);
       $scope.email = null;
     };
 
@@ -18,25 +17,20 @@ angular.module('gobeApp')
         var email = $scope.emailList.indexOf(email);
           $scope.emailList.splice(email, 1);
           $scope.newGroup.emailList = $scope.emailList;
-          console.log('inside confirm')
-          console.log($scope.newGroup.emailList)
       };
-        console.log('emailList outside confirm')
-        console.log($scope.newGroup.emailList)
     };
 
 
     $scope.updateGroup = function addGroup(form) {
       var data = $scope.newGroup;
-      console.log(data)
       $scope.submitted = true;
          if(form.$valid){
              Group.update({id: $stateParams.id }, data,
                function(data){
-                  $state.go('group.list')
+                  $state.go('group.list');
                  }),
                  function(err){
-                  $scope.addEventError = "Looks like something went wrong! Please try again"
+                  $scope.addEventError = "Looks like something went wrong! Please try again";
                  }
                }
          else{
