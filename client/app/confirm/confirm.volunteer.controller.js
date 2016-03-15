3a59e0209570eaf8d01236f61dcbc82e8689d192
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-03-15 16:09:57 -0700

    comment out front end console logs

--- client/app/confirm/confirm.volunteer.controller.js
+++ client/app/confirm/confirm.volunteer.controller.js
@@ -10,11 +10,10 @@ angular.module('gobeApp')
     $scope.confirm = false;
     $scope.reject = false;
 
-    console.log('confirmedEmails')
-    console.log(confirmedEmails)
-    console.log("stateParams response")
-    console.log($stateParams.response)
-
+    // console.log('confirmedEmails')
+    // console.log(confirmedEmails)
+    // console.log("stateParams response")
+    // console.log($stateParams.response)
 
     console.log($stateParams.email)
     if($stateParams.email && ($stateParams.response === 'yes')){
@@ -23,10 +22,10 @@ angular.module('gobeApp')
       $scope.confirm = true;
       var uniqueConfirmed = _.uniq(confirmedEmails)
       $scope.event.confirmedEmails = uniqueConfirmed;
-      console.log( $scope.event.confirmedEmails)
+      // console.log( $scope.event.confirmedEmails)
       Event.update({id: $stateParams.event_id}, $scope.event, function(data){
         console.log('data');
-        console.log(data);
+        // console.log(data);
       })
     }
     else if($stateParams.email && ($stateParams.response === 'no')){
@@ -36,35 +35,10 @@ angular.module('gobeApp')
       var uniqueRejected = _.uniq(rejectedEmails)
       $scope.reject = true;
       $scope.event.rejectedEmails = uniqueRejected;
-      console.log($scope.event.rejectedEmails)
+      // console.log($scope.event.rejectedEmails)
       Event.update({id: $stateParams.event_id}, $scope.event, function(data){
-        console.log('data');
-        console.log(data);
+        // console.log('data');
+        // console.log(data);
       })
     }
-
-
-    // $scope.confirmGroupTime = function(date, time){
-    //   var confirmTime = time.replace(" and ", "-");
-    //   $scope.event.confirmGroup = $stateParams.group_id;
-    //   $scope.event.confirmDate = date;
-    //   $scope.event.confirmTime = confirmTime;
-
-    //   //if confirmGroup, Date, Time, doesn't exist
-    //   Event.update({id: $stateParams.event_id }, $scope.event,
-    //     function(data){
-    //       console.log("data")
-    //       console.log(data)
-    //       $scope.confirmGroup = true;
-    //        // $state.go('event.list')
-    //       }),
-    //       function(err){
-    //         // $scope.addEventError = "Looks like something went wrong! Please try again"
-    //       }
-
-    // }
-
-
-
-
   });
