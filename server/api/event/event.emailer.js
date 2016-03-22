f29d87edc660e1000ae0234d70369c2adfca7fe8
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-03-22 10:08:49 -0700

    change to event.emailer

--- server/api/event/event.emailer.js
+++ server/api/event/event.emailer.js
@@ -136,25 +136,26 @@ function volunteerMatch(event, host){
 			      to:  value, //looping over the email list
 			      from: 'hello@gobethe1.com',
 			      subject: 'Are you available to volunteer?',
-			      html:  '<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr>' +
-			    '<td align="center" width="50%">' +
-			    '<p style="font-size:14px;font-family:sans-serif;">You have been invited by ' + capFirstName + ' ' + capLastName + ' to  <br>' +
-			    'join the rest of the ' + capOrgName + ' for the move <br>' +
-			    'in party in your area on  <span style="font-weight:bold"> ' + finalDate + ' at ' + event.confirmTime + '</span>. <br>' +
-			    'Can you make it? <br><br>' +
-			    '<a href=' + linkConfirm +  ' style="background-color:#4A90E2;border:1px solid #4A90E2;border-radius:5px;color:#ffffff ;display:inline-block;font-family:sans-serif;font-size:14px;line-height:44px;text-align:center;text-decoration:none;width:40%;-webkit-text-size-adjust:none;mso-hide:all;">Yes, I\'ll be there</a><br><br>' +
-			    '<a href=' + linkReject +  '  style="text-decoration:underline;color:black;font-size:14px;">I can\'t make it</a></p></td>' +
-			    '<td align="left" width="50%" style="vertical-align:top;">' +
-			    '<p style="font-size:14px;font-family:sans-serif;font-weight:bold;">What\'s this invite about?</p>' +
-			    '<p style="font-size:14px;font-family:sans-serif">Someone just moved off the streets and it\'s <br>' +
-			   	'time to party! This person now lives in your <br>' +
-			   	'area and you have been invited to help <br>' +
-			   	'welcome them home! Ready to make a difference? <br>' +
-			   	'Simply, accept the invite, ask your group leader <br>' +
-			   	'what items you can bring, and bring the items <br>' +
-			   	'with you to the party!</p></td>' +
-			    '</tr></table>'
-
+			      html:
+					    '<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr>' +
+					    '<td align="center" width="50%">' +
+					    '<p style="font-size:14px;font-family:sans-serif;">You have been invited by ' + capFirstName + ' ' + capLastName + ' to  <br>' +
+					    'join the rest of the ' + capOrgName + ' for the move <br>' +
+					    'in party in your area on  <span style="font-weight:bold"> ' + finalDate + ' at ' + event.confirmTime + '</span>. <br>' +
+					    'Can you make it? <br><br>' +
+					    '<a href=' + linkConfirm +  ' style="background-color:#4A90E2;border:1px solid #4A90E2;border-radius:5px;color:#ffffff ;display:inline-block;font-family:sans-serif;font-size:14px;line-height:44px;text-align:center;text-decoration:none;width:40%;-webkit-text-size-adjust:none;mso-hide:all;">Yes, I\'ll be there</a><br><br>' +
+					    '<a href=' + linkReject +  '  style="text-decoration:underline;color:black;font-size:14px;">I can\'t make it</a></p></td>' +
+					    '<td align="left" width="50%" style="vertical-align:top;">' +
+					    '<p style="font-size:14px;font-family:sans-serif;font-weight:bold;">What\'s this invite about?</p>' +
+					    '<p style="font-size:14px;font-family:sans-serif">Someone just moved off the streets and it\'s <br>' +
+					   	'time to party! This person now lives in your <br>' +
+					   	'area and you have been invited to help <br>' +
+					   	'welcome them home! Ready to make a difference? <br>' +
+					   	'Simply, accept the invite, ask your group leader <br>' +
+					   	'what items you can bring, and bring the items <br>' +
+					   	'with you to the party!</p></td>' +
+					    '</tr>' +
+					    '</table>'
 			    };
 
 				// if(index === -1){
@@ -204,12 +205,12 @@ function confirmGroup(event, host){
 			  },
 			  function(user, group, done) {
 
-			  	      	var groupContact = user.email;
+			  	      var groupContact = user.email;
 			  	    	var capFirstName = _.capitalize(event.firstName);
 			  	    	var capLastName = _.capitalize(event.lastName);
-			  	    	var dateString = event.confirmDate.toString()
-			  	      	var finalDate = dateString.slice(0, 10)
-			  	      	var capOrgName = group.organizationName.capitalize();
+			  	    	var dateString = event.confirmDate.toString();
+			  	      var finalDate = dateString.slice(0, 10);
+			  	      var capOrgName = group.organizationName.capitalize();
 
 			  		    var transporter = nodemailer.createTransport({
 			  		      host: GodaddySMTP,
@@ -224,31 +225,32 @@ function confirmGroup(event, host){
 			  		      to: groupContact, //admin annie email
 			  		      from: 'hello@gobethe1.com',
 			  		      subject: capFirstName + '\'s Move-In Party',
-			  		      html: '<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr>' +
-			      			    '<td align="left" width="50%">' +
-			      			    '<img src="https://s3-us-west-1.amazonaws.com/gobethe1-prod/confirm-email-logo.png">' +
-			      			    '<p style="font-size:16px;font-family:sans-serif;">Get ready to party!</p>' +
-			      			    '<p style="font-size:14px;font-family:sans-serif;font-weight:bolder">Details</p>' +
-			      			    '<p style="font-size:14px;font-family:sans-serif;">The ' + capOrgName + ' are confirmed for the move in party<br>' +
-			      			    'of ' + capFirstName + ' ' + capLastName + ' on  <span style="font-weight:bolder"> ' + finalDate + ' at ' + event.confirmTime + '</span></p>' +
-			      			   	'<p style="font-size:14px;font-family:sans-serif;font-weight:bolder">Event Address</p>' +
-			      			    '<p style="font-size:14px;font-family:sans-serif;"> ' + event.address + ' </p>' +
-			      			    '<p style="font-size:14px;font-family:sans-serif;font-weight:bolder;">What to bring?</p>' +
-			      			    '<p style="font-size:14px;font-family:sans-serif;">Download the checklist of items <a href="https://s3-us-west-1.amazonaws.com/gobethe1-prod/welcome-kit.pdf" target="_blank" style="text-decoration:underline;">here</a>. This is a typical <br>' +
-			      			    'GOBE Welcome Home Kit. You can bring more but we <br>' +
-			      			    'ask you bring this as a minimum. All items except<br>' +
-			      			    'pillows can be lightly used.</p>' +
-			      			    '<br><p style="font-size:14px;font-family:sans-serif;">See you there,</p>' +
-			      			    '<p style="font-size:14px;font-family:sans-serif;">GOBE team</p>' +
-			      			    '</td>' +
-			      			    '<td align="left" width="50%">' +
-			      			    '<p style="font-size:14px;font-family:sans-serif;font-weight:bolder">Some nice touches</p>' +
-			      			    '<p style="font-size:14px;font-family:sans-serif">Check out instagram <a href="https://www.instagram.com/gobethe1/" target="_blank">@GOBETHE1</a> for good ideas<br>' +
-			      			   	'from previous parties. Try to think of things you typically find <br>' +
-			      			   	'at a party like food and drinks. Things such as welcome home banners <br>' +
-			      			   	'and posters make each party an extra special event!<p>' +
-			      			   	'</td>' +
-			      			    '</tr></table>'
+			  		      html:
+		  		      		'<table width="100%" border="0" cellspacing="0" cellpadding="0"><tr>' +
+		      			    '<td align="left" width="50%">' +
+		      			    '<img src="https://s3-us-west-1.amazonaws.com/gobethe1-prod/confirm-email-logo.png">' +
+		      			    '<p style="font-size:16px;font-family:sans-serif;">Get ready to party!</p>' +
+		      			    '<p style="font-size:14px;font-family:sans-serif;font-weight:bolder">Details</p>' +
+		      			    '<p style="font-size:14px;font-family:sans-serif;">The ' + capOrgName + ' are confirmed for the move in party<br>' +
+		      			    'of ' + capFirstName + ' ' + capLastName + ' on  <span style="font-weight:bolder"> ' + finalDate + ' at ' + event.confirmTime + '</span></p>' +
+		      			   	'<p style="font-size:14px;font-family:sans-serif;font-weight:bolder">Event Address</p>' +
+		      			    '<p style="font-size:14px;font-family:sans-serif;"> ' + event.address + ' </p>' +
+		      			    '<p style="font-size:14px;font-family:sans-serif;font-weight:bolder;">What to bring?</p>' +
+		      			    '<p style="font-size:14px;font-family:sans-serif;">Download the checklist of items <a href="https://s3-us-west-1.amazonaws.com/gobethe1-prod/welcome-kit.pdf" target="_blank" style="text-decoration:underline;">here</a>. This is a typical <br>' +
+		      			    'GOBE Welcome Home Kit. You can bring more but we <br>' +
+		      			    'ask you bring this as a minimum. All items except<br>' +
+		      			    'pillows can be lightly used.</p>' +
+		      			    '<br><p style="font-size:14px;font-family:sans-serif;">See you there,</p>' +
+		      			    '<p style="font-size:14px;font-family:sans-serif;">GOBE team</p>' +
+		      			    '</td>' +
+		      			    '<td align="left" width="50%">' +
+		      			    '<p style="font-size:14px;font-family:sans-serif;font-weight:bolder">Some nice touches</p>' +
+		      			    '<p style="font-size:14px;font-family:sans-serif">Check out instagram <a href="https://www.instagram.com/gobethe1/" target="_blank">@GOBETHE1</a> for good ideas<br>' +
+		      			   	'from previous parties. Try to think of things you typically find <br>' +
+		      			   	'at a party like food and drinks. Things such as welcome home banners <br>' +
+		      			   	'and posters make each party an extra special event!<p>' +
+		      			   	'</td>' +
+		      			    '</tr></table>'
 			  		    };
 
 			  		    transporter.sendMail(mailOptions, function(err) {
