2db531e1e45e3fa231051f0f94c95f79321c3480
Greg Rock <grock006@gmail.com>
2016-03-24 11:55:54 -0700

    added profile routes and controller

---
+++ client/app/profile/profile.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('gobeApp')
+  .controller('ProfileCtrl', function ($scope) {
+    $scope.message = 'Hello';
+  });
---
+++ client/app/profile/profile.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('gobeApp')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('profile', {
+        url: '/profile',
+        templateUrl: 'app/profile/profile.html',
+        controller: 'ProfileCtrl'
+      });
+  });
