255866590ad914b870a74b59e546634f30700d1d
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-03-25 14:59:59 -0700

    add footer to components, create footer controller, inject state, add go to links in footer for current set ups / users

---
+++ client/components/footer/footer.controller.js
@@ -0,0 +1,23 @@
+'use strict';
+
+angular.module('gobeApp')
+  .controller('FooterCtrl', function ($scope, $state, $location, Auth) {
+
+    // $scope.isCollapsed = true;
+    $scope.isLoggedIn = Auth.isLoggedIn;
+    $scope.isAdmin = Auth.isAdmin;
+    // $scope.getCurrentUser = Auth.getCurrentUser;
+
+    $scope.causesLink = function(){
+      console.log('hello')
+      if(Auth.isAdmin()){
+        $state.go('event.list');
+      } else {
+        $state.go('profile.event.list');
+      }
+    };
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
