2fa1114a7b78e8df0cc39c732411d4bdc072c670
Greg Rock <grock006@gmail.com>
2016-04-08 12:59:40 -0700

    fixing stripe form validations

--- client/components/modal/modal.controller.js
+++ client/components/modal/modal.controller.js
@@ -8,35 +8,40 @@ angular.module('gobeApp')
     $scope.promo;
     $scope.currentUser = currentUser;
     $scope.cardErrorMessage = null;
-    $scope.checkoutForm;
+    // $scope.checkoutForm;
 
     $scope.cancel = function () {
       $uibModalInstance.dismiss();
     };
 
+    console.log($scope.checkoutForm)
+
     console.log('stripe callback outside submit: ', $scope.stripeCallback)
-    $scope.stripeCallback = function (code, result) {
-        $scope.submitted = true;
-        $scope.cardErrorMessage = null;
+        
 
-        console.log('scope checkout: ', $scope.checkoutForm)
-        if(!result.error) {
-           User.createSubscription({token: result.id, user_id: currentUser._id, email: currentUser.email, promo: $scope.promo},
-            function(data){
-                if(data.status === 'success'){
-                  $scope.currentUser.activeSubscription = true;
-                  $state.go('group.new');
-                }
-                else if(data.status === 'error'){
-                   $scope.cardErrorMessage = data.error.message;
-                }
-              }),
-              function(err){
-                console.log("err")
-                console.log(err)
-              }
-        }
+        $scope.stripeCallback = function (code, result) {
+            $scope.submitted = true;
+            $scope.cardErrorMessage = null;
 
-    };
+            console.log('scope checkout: ', $scope.checkoutForm)
+            if(!result.error) {
+               User.createSubscription({token: result.id, user_id: currentUser._id, email: currentUser.email, promo: $scope.promo},
+                function(data){
+                    if(data.status === 'success'){
+                      $scope.currentUser.activeSubscription = true;
+                      $state.go('group.new');
+                    }
+                    else if(data.status === 'error'){
+                       $scope.cardErrorMessage = data.error.message;
+                    }
+                  }),
+                  function(err){
+                    console.log("err")
+                    console.log(err)
+                  }
+            }
+
+        };
+
 
 }])
