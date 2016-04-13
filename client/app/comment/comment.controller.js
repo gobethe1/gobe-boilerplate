2d4c2e4019825f262f5278dbfeaa218a38958807
Greg Rock <grock006@gmail.com>
2016-04-13 12:00:30 -0700

    completed feedback story tweaks

--- client/app/comment/comment.controller.js
+++ client/app/comment/comment.controller.js
@@ -1,17 +1,19 @@
 'use strict';
 
 angular.module('gobeApp')
-  .controller('CommentCtrl', function ($scope, $state, Comment, currentUser, commentModel) {
+  .controller('CommentCtrl', function ($scope, $state, Comment, currentUser, commentModel, Auth) {
 
   	$scope.comment = {};
   	$scope.comments = commentModel;
   	$scope.user = currentUser;
   	$scope.comment.email = currentUser.email;
-
+  	$scope.isAdmin = Auth.isAdmin();
   
   	$scope.addFeedback = function(form){
+  		console.log(form)
   		$scope.submitted = true;
   		$scope.comment.createdAt = Date.now()
+  		$scope.comment.userId = $scope.user._id
 
   		if(form.$valid){
   			Comment.save($scope.comment, function(data){
