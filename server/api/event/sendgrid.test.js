426e9e254f7d9403dfa9560870079e587f824e42
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-04-26 18:21:43 -0700

    set up substitution to work for sendgrid template

--- server/api/event/sendgrid.test.js
+++ server/api/event/sendgrid.test.js
@@ -9,14 +9,18 @@ var User              = require('../user/user.model');
 var GoogleAPIKey      = process.env.GOOGLE_API_KEY;
 var SENDGRID_API_KEY  = process.env.SENDGRID_API_KEY;
 var sendgrid          = require('sendgrid')(SENDGRID_API_KEY);
+var date              = new Date();
 
 var sendEmail = function() {
     var email = new sendgrid.Email({
         to: 'cassie.purtlebaugh@gmail.com',
         from: 'gobethe1dev@gmail.com',
         subject: 'Testing testing 1.3.3',
-        html: '<h1>This is a mofo test</h1>'
+        html: '<h1>This is a mofo test. Today\'s date is: ',
+        // sub: {'<%date%>': new Date()},
     });
+    email.addFilter('templates', 'template_id', 'bc764a16-9e00-4cae-b42e-5e6fb11d282f');
+    email.addSubstitution("%date%", date)
 
     sendgrid.send(email, function(err, json) {
      if (err) { return console.error(err); }
