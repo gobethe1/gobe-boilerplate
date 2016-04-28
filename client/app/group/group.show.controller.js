5d8f92315bffcaf6f6820199841da4b5af7ce88f
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-04-28 14:31:11 -0700

    moved checkMatchedArray fx to run on ctrl vs view

--- client/app/group/group.show.controller.js
+++ client/app/group/group.show.controller.js
@@ -10,13 +10,32 @@ angular.module('gobeApp')
     console.log($scope.events)
     $scope.isAdmin = Auth.isAdmin;
 
-    
+
     //filters
     var groupId = groupShow._id;
     $scope.matchedZipCode =  {'confirmGroup': groupId, 'published': true};
 
-    $scope.checkMatchedArray = function(event, index) {
-      return event.published && !event.confirmGroup && userGroup.matchZipCodeArr.indexOf(event.zipCode) !== -1;
-    }
+
+    var checkMatchedArray = function(events){
+      var matchedArray    = [];
+      var unmatchedArray  = [];
+      var returnValue     = [matchedArray, unmatchedArray];
+
+      eventModel.forEach(function(event){
+        if (event.published && !event.confirmGroup && groupShow.matchZipCodeArr.indexOf(event.zipCode) !== -1){
+           matchedArray.push(event);
+        }
+        else {
+           unmatchedArray.push(event);
+        }
+      })
+      return returnValue;
+    };
+
+    console.log('checkMatchedArray: ', checkMatchedArray())
+
+    // $scope.checkMatchedArray = function(event, index) {
+    //   return event.published && !event.confirmGroup && userGroup.matchZipCodeArr.indexOf(event.zipCode) !== -1;
+    // }
 
   });
