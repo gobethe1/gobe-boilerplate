892fbca59151f55c612480a65ddd3cdf2da1eeed
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-04-28 17:04:15 -0700

    refactor group-show ctrl to loop through past and current matched values in causes arr

--- client/app/group/group.show.controller.js
+++ client/app/group/group.show.controller.js
@@ -15,22 +15,38 @@ angular.module('gobeApp')
     $scope.matchedZipCode =  {'confirmGroup': groupId, 'published': true};
 
     $scope.matchedArray    = [];
-    $scope.acceptedMatchedArray  = [];
-    $scope.returnValue     = [$scope.matchedArray, $scope.acceptedMatchedArray];
+    $scope.acceptedPastArray  = [];
+    $scope.acceptedCurrentArray = [];
+    $scope.returnValue     = [$scope.matchedArray, $scope.acceptedCurrentArray, $scope.acceptedPastArray];
+    $scope.pastArray       = [];
+    $scope.currentArray    = [];
 
     var checkMatchedArray = function(events){
         eventModel.forEach(function(event){
           if (event.published && !event.confirmGroup && groupShow.matchZipCodeArr.indexOf(event.zipCode) !== -1){
              $scope.matchedArray.push(event);
           }
-          else if(event.published && event.confirmGroup === groupId) {
-             $scope.acceptedMatchedArray.push(event);
+          else if(event.published && event.confirmGroup === groupId && event.confirmDate >= Date.now()) {
+             $scope.acceptedCurrentArray.push(event);
+          } else if (event.published && event.confirmGroup === groupId && event.confirmDate < Date.now()) {
+             $scope.acceptedPastArray.push(event);
+          } else {
+            return err
           }
         })
+        console.log($scope.returnValue)
         return $scope.returnValue;
     };
 
-    console.log('checkMatchedArray: ', checkMatchedArray(), 'matchedArray: ', $scope.matchedArray)
+
+
+
+
+    // console.log('matchedArray: ', $scope.matchedArray)
+    // console.log('aaccepted matched: ', $scope.acceptedMatchedArray)
+    // console.log('checkMatchedArray: ', checkMatchedArray())
+    // console.log('pastArray: ', $scope.pastArray, 'currentArray: ', $scope.currentArray)
+    // console.log('check if passed fx: ', checkIfEventPassed())
 
 
     // $scope.checkMatchedArray = function(event, index) {
