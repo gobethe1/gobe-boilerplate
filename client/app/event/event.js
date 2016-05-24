0b3def865b2d385b3cb82a3a59c472ea4837217a
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2016-05-24 15:53:28 -0700

    test commenting out groupeventGroup in event.js

--- client/app/event/event.js
+++ client/app/event/event.js
@@ -47,16 +47,16 @@ angular.module('gobeApp')
         resolve:{
           eventShow: function(Event, $stateParams){
             return Event.get({id: $stateParams.id}).$promise;
-          },
-          eventGroup: function(Group, eventShow){
-            if(eventShow.confirmGroup){
-              return Group.get({id: eventShow.confirmGroup }).$promise;
-            }
-            else{
-              return null;
-            }
           }
-        }
+        //   eventGroup: function(Group, eventShow){
+        //     if(eventShow.confirmGroup){
+        //       return Group.get({id: eventShow.confirmGroup }).$promise;
+        //     }
+        //     else{
+        //       return null;
+        //     }
+        //   }
+        // }
       })
 
        .state('event.edit', {
